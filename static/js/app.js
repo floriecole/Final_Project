71fb57af107c694ccc49b2fd0544852258e36a0a
floriecole <floriejrcole@gmail.com>
2019-07-19 15:04:37 -0400

    new update

--- static/js/app.js
+++ static/js/app.js
@@ -9,16 +9,25 @@ submit.on("click", function() {
   d3.event.preventDefault();
 
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#example-form-input");
+  var inputElement = d3.select("#search-form-input");
 
   // Get the value property of the input element
   var author = inputElement.property("value");
 
 //   console.log(inputValue);
 
-  // Set the span tag in the h1 element to the text
+// var filteredData = people.filter(person => person.bloodType === inputValue);
+ 
+// Set the span tag in the h1 element to the text
   // that was entered in the form
-  d3.select("h1>span").text(author);
+//   d3.select("h1>span").text(author);
+  d3.select(".summary").text(author);
+//   d3.select(".summary")
+//     .append("li").text(`Mean: ${mean}`)
+//     .append("li").text(`Median: ${median}`)
+//     .append("li").text(`Mode: ${mode}`)
+//     .append("li").text(`Variance: ${variance}`)
+//     .append("li").text(`Standard Deviation: ${standardDeviation}`);
 
   d3.json(`api/authorsearch/${author}`).then(function(data){
         console.log (data)
