c276c22c95d7693b41b9341001a3d09899e42083
floriecole <floriejrcole@gmail.com>
2019-07-19 21:14:01 -0400

    uploads

--- static/js/app.js
+++ static/js/app.js
@@ -6,13 +6,13 @@ var submit = d3.select("#submit");
 submit.on("click", function() {
 
   // Prevent the page from refreshing
-  d3.event.preventDefault();
+d3.event.preventDefault();
 
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#search-form-input");
+var inputElement = d3.select("#search-form-input");
 
   // Get the value property of the input element
-  var author = inputElement.property("value");
+var author = inputElement.property("value");
 
 //   console.log(inputValue);
 
@@ -20,8 +20,8 @@ submit.on("click", function() {
  
 // Set the span tag in the h1 element to the text
   // that was entered in the form
-//   d3.select("h1>span").text(author);
-  d3.select(".summary").text(author);
+d3.select("h1>span").text(author);
+//   d3.select(".summary").text(author);
 //   d3.select(".summary")
 //     .append("li").text(`Mean: ${mean}`)
 //     .append("li").text(`Median: ${median}`)
@@ -29,8 +29,8 @@ submit.on("click", function() {
 //     .append("li").text(`Variance: ${variance}`)
 //     .append("li").text(`Standard Deviation: ${standardDeviation}`);
 
-  d3.json(`api/authorsearch/${author}`).then(function(data){
-        console.log (data)
+d3.json(`api/authorsearch/${author}`).then(function(data){
+    // console.log (data)  
 });
 
 // author='Amy Tan'
